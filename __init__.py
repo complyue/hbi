516e22b49a72d34ce404a71df985f8d009d33f32
Compl Yue <compl.yue@icloud.com>
2019-05-07 04:01:53 +0800

    reorganize folder structure

--- __init__.py
+++ __init__.py
@@ -1,61 +1,14 @@
 """
-Hosting Based Interfacing
-
-    HBI is an alternative idiomatic methodology to implement IPC (inter-process communication), it tends to be
-    adapting amongst RPC(remote procedure call), Streaming/Pipelining, and PubSub/Notification styles of communication
-    at runtime, and typically leverage a single network channel (sockets, pipes, http, websockets, etc.) between two
-    peer nodes.
+Hosting Based Interface
 
 """
-from .aio import *
-from .buflist import *
-from .bytesbuf import *
-from .co import *
-from .conn import *
-from .context import *
-from .ho import *
-from .interop import *
-from .log import *
-from .po import *
-from .proto import *
-from .sendctrl import *
+from .pkg import *
 
 __all__ = [
 
-    # exports from .aio
-    'CancellableQueue', 'handle_signals',
-
-    # exports from .buflist
-    'BufferList',
-
-    # exports from .bytesbuf
-    'BytesBuffer',
-
-    # exports from .co
-    'Conver',
-
-    # exports from .conn
-    'HBIC', 'HBIS',
-
-    # exports from .context
-    'run_in_context',
-
-    # exports from .ho
-    'HostingEnd',
-
-    # exports from .interop
-    'null', 'true', 'false', 'nan', 'NaN', 'JSOND',
-
-    # exports from .log
-    'hbi_root_logger', 'get_logger',
-
-    # exports from .po
-    'PostingEnd',
-
-    # exports from .proto
-    'PACK_HEADER_MAX',
-
-    # exports from .sendctrl
-    'SendCtrl',
+    # exports from .pkg
+    'CancellableQueue', 'handle_signals', 'BufferList', 'BytesBuffer', 'Conver', 'HBIC', 'HBIS', 'run_in_context',
+    'HostingEnd', 'null', 'true', 'false', 'nan', 'NaN', 'JSOND', 'root_logger', 'get_logger', 'PostingEnd',
+    'PACK_HEADER_MAX', 'SendCtrl',
 
 ]
