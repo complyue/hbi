4067bf9396652219075ffed4f72d9a9fef74fa0d
Compl Yue <compl.yue@icloud.com>
2019-05-13 02:17:41 +0800

    unify py he with go

--- pkg/he/__init__.py
+++ pkg/he/__init__.py
@@ -1,12 +1,11 @@
 """
-Hosting Environment
+implements the hosting environment to land peer scripts for HBI
 
 """
 from .he import *
 
 __all__ = [
-
     # exports from .he
-    'HostingEnv', 'run_py',
-
+    "HostingEnv",
+    "run_py",
 ]
